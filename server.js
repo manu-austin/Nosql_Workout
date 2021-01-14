f6148373aceedb0c694666d5e5a2b8eca0dc7e55
Emmanuel Durand <shernua@gmail.com>
2021-01-14 16:42:13 -0600

    deploying

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/workout_db", {
 
 // using html and api routes 
 app.use(require("./routes/api-routes.js"));
-app.use(require("./routes/html-routes"));
+app.use(require("./routes/html-routes.js"));
 
 
 app.listen(PORT, () => {
